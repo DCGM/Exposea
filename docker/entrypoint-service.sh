4935d1c023efeeecd2a60102ec998b6a7ea6da9c
dejvax <ikarus12111@gmail.com>
2025-07-08 14:30:07 +0200

    Added service docker

---
+++ docker/entrypoint-service.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Path to the directory containing subfolders
+INPUT_DIR="/data"
+OUTPUT_DIR="/output"
+
+while true; do
+  FOLDERS=("$INPUT_DIR"/*/)
+  for dir in FOLDERS=; do
+        [ -d "$dir" ] || continue
+        base="$(basename "$dir")"
+
+        if [[ "$base" != *_processing && "$base" != *_done ]]; then
+            echo "Found: $base at $dir"
+
+            PROCESS_DIR="$INPUT_DIR/$base"
+
+            mv "$dir" "${PROCESS_DIR}_processing"
+
+            echo "Processing: ${PROCESS_DIR}_processing"
+            python3 register.py --output $OUTPUT_DIR --input "${PROCESS_DIR}_processing"
+
+            mv "${PROCESS_DIR}_processing" "${PROCESS_DIR}_done"
+            echo "Done: ${PROCESS_DIR}_done"
+        fi
+    done
